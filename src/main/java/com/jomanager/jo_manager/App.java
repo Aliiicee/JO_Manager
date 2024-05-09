5872cc6e304ca9647243258e098d19503e20f050
Alice <alice.devallombreuse@gmail.com>
2024-05-09 18:59:11 +0200

    Menu and Home fxmls

--- src/main/java/com/jomanager/jo_manager/App.java
+++ src/main/java/com/jomanager/jo_manager/App.java
@@ -1,7 +1,6 @@
 package com.jomanager.jo_manager;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,7 +8,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/jomanager/jo_manager/Controllers/MenuController.java
@@ -0,0 +1,19 @@
+package com.jomanager.jo_manager.Controllers;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+    public Button home_btn;
+    public Button athletes_btn;
+    public Button sports_btn;
+    public Button events_btn;
+    public Button results_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
